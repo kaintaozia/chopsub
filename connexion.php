ed7b8d1df3e0a8d9edd7f7429675e144b1f3029d
kaintaozia <kaintaozia@gmail.com>
2019-04-12 10:22:43 +0200

    Update connexion.php

--- connexion.php
+++ connexion.php
@@ -4,7 +4,7 @@
 		{
 			try
 			{
-				$bdd = new PDO('mysql:host=localhost;dbname=chopsub', 'chopsub', 'chopsub');
+				$bdd = new PDO('mysql:host=eu-cdbr-west-02.cleardb.net;dbname=heroku_880ea39488893f3', 'b9cfd46243c65b', '8480309c');
 				return $bdd;
 			}
 				catch (Exception $e)
